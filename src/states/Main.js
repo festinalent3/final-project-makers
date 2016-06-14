3a0b898b008b082bb90d1c94a37744080fb5d34c
Omar Alvarez <omajul2@hotmail.com>
2016-06-14 12:44:06 +0200

    Add spaceship, bullets and enemies

--- src/states/Main.js
+++ src/states/Main.js
@@ -4,85 +4,99 @@ import move from '../modules/moves';
 import makeMany from '../modules/makeMany';
 // import collectStar from 'modules/collectStar';
 
-var dude;
+var spacefield;
+var backgroundv;
+var player;
 var cursors;
-var platforms;
-var stars;
-var score = 0;
-
+var bullets;
+var bulletTime = 0;
+var fireButton;
+var enemies;
 
 class Main extends Phaser.State {
 
 	create() {
+    // Set physics for the groups
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		//  We're going to be using physics, so enable the Arcade Physics system
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  A simple background for our game
-    this.game.add.sprite(0,100, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.game.add.group();
-
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
-
-    // Here we create the ground.
-    var ground = platforms.create(0, this.game.world.height - 210, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    dude = new Dude(this.game);
-    this.game.physics.arcade.enable(dude);
-    setProperties(dude);
-
-    //  Finally some stars to collect
-    stars = this.game.add.group();
-    // We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-    makeMany(stars, 12);
-
-    // //  The score
-    this.scoreText = this.game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#fff' });
-
-    //  Our controls.
+		spacefield = this.game.add.tileSprite(0,0,800,600,"starfield");
+    backgroundv = 5;
+    player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 200, 'player');
+    
+    // Set physics for spaceship
+    this.game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
     cursors = this.game.input.keyboard.createCursorKeys();
+
+    bullets = this.game.add.group();
+    bullets.enableBody = true;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+
+    fireButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    enemies = this.game.add.group();
+    enemies.enableBody = true;
+
+    createEnemies(this.game);
+
+    function createEnemies(game) {
+    	for(var y = 0; y < 4; y++){
+    		for(var x = 0; x < 10; x++){
+    			var enemy = enemies.create(x*48,y*50, 'enemy');
+    			enemy.anchor.setTo(0.5,0.5);
+    		}
+    	}
+    	enemies.x = 100;
+    	enemies.y = 50;
+
+    	var tween = game.add.tween(enemies).to({x:200},2000,Phaser.Easing.Linear.None,true,0,1000,true)
+    	tween.onLoop.add(descend, this);
+    }
+
+    function descend() {
+    	enemies.y += 10;
+    }
+		
   }
 
   update() {
+  	this.game.physics.arcade.overlap(bullets, enemies, collisionHandler, null, this);
+    spacefield.tilePosition.y += backgroundv;
 
-  	this.game.physics.arcade.collide(dude, platforms);
-  	this.game.physics.arcade.collide(stars, platforms);
+    player.body.velocity.x = 0;
 
-  	move(dude, cursors);
+    if(cursors.left.isDown) {
+    	player.body.velocity.x = -350;
+    }
+    if(cursors.right.isDown) {
+        player.body.velocity.x = 350;
+    }
+    if(fireButton.isDown) {
+        fireBullet(this.game);
+    }
+  
+	  function fireBullet(game) {
+	    if(game.time.now > bulletTime){
+	        var bullet = bullets.getFirstExists(false);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.game.physics.arcade.overlap(dude, stars, collectStar, null, this);
-
-    function collectStar(player, star) {
-
-				// Removes the star from the screen
-				star.kill();
-
-				//  Add and update the score
-				score += 10;
-				this.scoreText.text = 'Score: ' + score;
-
-			}
-
+	        if(bullet){
+	            bullet.reset(player.x + 14,player.y);
+	            bullet.body.velocity.y = -400;
+	            bulletTime = game.time.now + 200;
+	        }
+	    }
 		}
+
+		function collisionHandler(bullet,enemy){
+			bullet.kill();
+			enemy.kill();
+		}
+	}
+
 }
 
 export default Main;
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -1,10 +1,10 @@
 class Preload extends Phaser.State {
 
 	preload() {
-		this.game.load.image('sky', 'assets/sky.png');
-		this.game.load.image('ground', 'assets/platform.png');
-		this.game.load.image('star', 'assets/star.png');
-		this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);		
+		this.game.load.image('starfield', "assets/background.png");
+    this.game.load.image('player', "assets/spaceship.png")
+    this.game.load.image('bullet', "assets/bullet.png")
+    this.game.load.image('enemy', "assets/star.png")
 	}
 
 	create() {
