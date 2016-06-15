aee39361dd60c07e8bb00e42cc394299419f9f82
hnamitha1 <namitha.h@gmail.com>
2016-06-15 14:11:52 +0100

    lives added

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -1,5 +1,7 @@
 var intersecting = false;
+var attackReceived = false;
 var score = 0;
+var lives = 3;
 
 export function collision(bullet,enemy){
 	intersecting = true;
@@ -14,3 +16,20 @@ export function calculateScore(scoreText) {
 		intersecting = false;
 	}
 };
+
+export function killPlayer(player, bullet){
+	attackReceived = true;
+	bullet.kill();
+	if (lives <= 1){
+    player.kill();
+  }
+}
+
+export function LifeScore(lifeText) {
+	if (attackReceived){
+	  lives -= 1;
+	  lifeText.text = 'Lives: ' + lives;
+	  attackReceived = false;
+	} 
+};
+
--- src/states/Main.js
+++ src/states/Main.js
@@ -12,57 +12,56 @@ var bullets;
 var enemyBullets;
 var enemies;
 var scoreText;
-var enemyBullet
+var enemyBullet;
+var lifeText;
+var stateText;
 
 class Main extends Phaser.State {
 
-	create() {
-		// Set physics for the groups
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  create() {
+    // Set physics for the groups
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		spacefield = this.game.add.tileSprite(0,0,800,600,"starfield");
-		backgroundVelocity = 5;
-		player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 200, 'player');
+    spacefield = this.game.add.tileSprite(0,0,800,600,"starfield");
+    backgroundVelocity = 5;
+    player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 200, 'player');
 
-		scoreText = this.game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
+    scoreText = this.game.add.text(16, 16, 'Score: 0', { font: '32px Arial', fill: '#FFF' });
 
-		// Set physics for spaceship
-		this.game.physics.arcade.enable(player);
-		player.body.collideWorldBounds = true;
-		cursors = this.game.input.keyboard.createCursorKeys();
+    lifeText = this.game.add.text(16, 56, 'Lives: 3', { font: '32px Arial', fill: '#fff' });
 
-		// Player bullets
-		bullets = this.game.add.group();
-		set.bulletsProperties(bullets, 30, 'bullet');
+    // Set physics for spaceship
+    this.game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    cursors = this.game.input.keyboard.createCursorKeys();
 
-		// Enemy bullets
-		enemyBullets = this.game.add.group();
-		set.bulletsProperties(enemyBullets, 3, 'enemyBullet');
+    // Player bullets
+    bullets = this.game.add.group();
+    set.bulletsProperties(bullets, 30, 'bullet');
 
-		enemies = this.game.add.group();
-		enemies.enableBody = true;
-		createEnemies(this.game, enemies);
-	}
+    // Enemy bullets
+    enemyBullets = this.game.add.group();
+    set.bulletsProperties(enemyBullets, 3, 'enemyBullet');
 
-	update() {
+    enemies = this.game.add.group();
+    enemies.enableBody = true;
+    createEnemies(this.game, enemies);
+  }
 
-		if(player.alive) {
-			move(player, cursors, this.game);
-			fire.ship(bullets, player, this.game);
-		}
+  update() {
 
-		fire.enemy(enemyBullets, enemies, this.game, player);
-		this.game.physics.arcade.overlap(enemyBullets, player, killPlayer, null, this);
+    if(player.alive) {
+      move(player, cursors, this.game);
+      fire.ship(bullets, player, this.game);
+      fire.enemy(enemyBullets, enemies, this.game, player);
+    }
 
-		function killPlayer(player, bullet) {
-			//explode player
-			player.kill()
-		}
+    this.game.physics.arcade.overlap(enemyBullets, player, handler.killPlayer, handler.LifeScore(lifeText), this);
 
-		this.game.physics.arcade.overlap(bullets, enemies, handler.collision, handler.calculateScore(scoreText), this);
-		spacefield.tilePosition.y += backgroundVelocity;
+    this.game.physics.arcade.overlap(bullets, enemies, handler.collision, handler.calculateScore(scoreText), this);
+    spacefield.tilePosition.y += backgroundVelocity;
 
-	}
+  }
 
 }
 
