83053b629e508394f7dff06611152cf2f4b1666d
Aday Mesa <adaymesagc@gmail.com>
2016-06-17 13:50:59 +0100

    Life module tested

--- test/lifeSpec.js
+++ test/lifeSpec.js
@@ -32,46 +32,22 @@ describe('life', function() {
     life.reset();
     expect(life.get()).toEqual(3);
   });
+
+    it('reduce life to 0', function(){
+    	for(var i = 0; i < 3; i++) 
+    	{
+    	life.reduce(player, bullet);
+    	life.count(lifeText);
+  		}
+    expect(life.get()).toEqual(0);
+  });
+
+  it('kills the player when life is 0', function(){
+    	for(var i = 0; i < 3; i++) 
+    	{
+    	life.reduce(player, bullet);
+    	life.count(lifeText);
+  		}
+    expect(player.kill).toHaveBeenCalled();
+  });
 });
-
-  // it('kills bullet and enemy', function(){
-  //   score.update(bullet, enemy);
-  //   expect(bullet.kill).toHaveBeenCalled();
-  //   expect(enemy.kill).toHaveBeenCalled();
-  //   score.reset();
-  // });
-
-  // it('increases the score', function(){
-  //   score.update(bullet, enemy);
-  //   expect(score.get()).toEqual(scoreIncrease);
-  //   score.reset();
-  // });
-
-
-// var attackReceived = false;
-// var lives = 3;
-
-// export function reduce(player, bullet){
-// 	attackReceived = true;
-// 	bullet.kill();
-// 	if (lives < 1){
-// 		player.kill();
-// 	}
-// };
-
-// export function count(lifeText) {
-// 	if (attackReceived){
-// 		lives -= 1;
-// 		lifeText.text = 'Lives: ' + lives;
-// 		attackReceived = false;
-// 	}
-// };
-
-// export function get(){
-// 	return lives;
-// }
-
-// export function reset(){
-// 	lives = 3;
-// 	attackReceived = false;
-// }
