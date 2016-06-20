c974cd18bf55a3b385435cf1f3bedb6a00a94d13
Aday Mesa <adaymesagc@gmail.com>
2016-06-20 11:26:08 +0100

    Refactoring nested conditions

--- src/modules/sound.js
+++ src/modules/sound.js
@@ -5,14 +5,8 @@ export function toggle(game, soundText) {
 	muteButton = game.input.keyboard.addKey(Phaser.Keyboard.M);
 
 	if(muteButton.isDown) {
-		if(toggler = !toggler){
-
-			game.sound.mute = toggler;
-			soundText.text = 'Sound: off';
-		} else {
-			toggler = !!toggler;
-			game.sound.mute = toggler;
-			soundText.text = 'Sound: on';
-		}
+		toggler = !toggler;
+		game.sound.mute = toggler;
+		soundText.text = (toggler) ? "Sound: Off" : "Sound: On";
 	}
 }
