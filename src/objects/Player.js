5f3b7b08053c777e3632e109590f5ba0412f975a
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-22 00:29:15 +0200

    unit tests for player.fire() almost done

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,5 +1,4 @@
 var bulletTime = 0;
-var fireButton;
 
 class Player {
 
@@ -7,47 +6,48 @@ class Player {
 		this.game = game;
 		this.player = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'player');
 		game.physics.arcade.enable(this.player);
-	};    
+	};
 
 	enableShip(){
 		this.player.body.collideWorldBounds = true;
 		this.player.animations.add('left', [0, 1, 2, 3, 4], 15, true);
 		this.player.animations.add('right', [0, 1, 2, 3, 4], 15, true);
-		fireButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-	}
+		this.fireButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	};
 
 	isAlive(player){
 		player = player || this.player;
 		return player.alive;
-	}
+	};
 
 	get ship(){
 		return this.player;
-	}
+	};
 
-	fire(bullets, laser,fireButton) {
+	fire(bullets, laser, fireButton, game, player) {
 		fireButton = fireButton || this.fireButton;
+		game = game || this.game;
+		player = player || this.player;
+
 	  if(fireButton.isDown) {
-	    this.fireBullet(bullets);
+	    this.fireBullet(bullets, game, player);
 	    laser.play();
 	    return true;
 	  }
-	}
+	};
 
 	fireBullet(bullets, game, player) {
-		game = game || this.game;
-		player = player || this.player;
-		
-	  if(this.game.time.now > bulletTime){
+	  if(game.time.now > bulletTime){
 	    var bullet = bullets.getFirstExists(false);
 
 	    if(bullet){
-	      bullet.reset(this.player.x + 14, this.player.y);
+	      bullet.reset(player.x + 14, player.y);
 	      bullet.body.velocity.y = -400;
-	      bulletTime = this.game.time.now + 200;
+	      bulletTime = game.time.now + 200;
 	    }
 	  }
-	}
+	};
+
 }
 
 export default Player;
--- test/PlayerSpec.js
+++ test/PlayerSpec.js
@@ -4,96 +4,133 @@ describe('Player', function(){
 	var game;
 	var player;
 
-  beforeEach(function(){
-    game = {
-      world: function(x){
-        return x;
-      },
-      add: {
-        sprite: function(x,y,key) {
-          return key;
-        }
-      },
-      physics : {
-        arcade: {
-          enable: function(x){
-              return x;
-          }
-        }
-      }
-    };
+	beforeEach(function(){
+		game = {
+			world: function(x){
+				return x;
+			},
+			add: {
+				sprite: function(x,y,key) {
+					return key;
+				}
+			},
+			physics : {
+				arcade: {
+					enable: function(x){
+						return x;
+					}
+				}
+			}
+		};
 
-    spyOn(game.add, 'sprite');
-    player = new Player(game);
-  });
+		spyOn(game.add, 'sprite');
+		player = new Player(game);
+	});
 
-  describe('initialization', function(){
-    it('creates a player sprite', function(){
-     	expect(game.add.sprite).toHaveBeenCalled();
-    });
-  });
+	describe('initialization', function(){
+		it('creates a player sprite', function(){
+			expect(game.add.sprite).toHaveBeenCalled();
+		});
+	});
 
-  describe('isAlive', function(){
-    var newPlayer;
+	describe('isAlive', function(){
+		var newPlayer;
 
-    beforeEach(function(){
-      player = {
-          alive: true
-      }
-      newPlayer = new Player(game);
-    });
+		beforeEach(function(){
+			player = {
+				alive: true
+			}
+			newPlayer = new Player(game);
+		});
 
-    it('returns if player is alive or not', function(){
-      expect(newPlayer.isAlive(player)).toEqual(true);
+		it('returns if player is alive or not', function(){
+			expect(newPlayer.isAlive(player)).toEqual(true);
 			player.alive = false;
 			expect(newPlayer.isAlive(player)).toEqual(false);
-    });
-  });
+		});
+	});
 
-  describe('fire and fireBullets', function(){
-    var anotherPlayer;
-    var fireButton;
-    var bullets;
-    var gameAudio;
-    var gameTime;
+	describe('#fire, indirectly testing #fireBullet', function(){
 
-    beforeEach(function(){
-      fireButton = {
-        isDown: true
-      },
-      bullets = {
-        getFirstExists: function(value){
-          return value;
-        }
-      },
-      gameAudio = {
-        add: {
-          audio: function(key) {
-            return key;
-          }
-        },
-      },
-      gameTime = {
-        time: {
-          now : 10
-        }
-      }
-      anotherPlayer = new Player(game);
-      spyOn(gameAudio.add, 'audio');
-      spyOn(bullets, 'getFirstExists');
-      spyOn(gameTime.time, 'now' );
-    });
+		var anotherPlayer;
+		var fireButton;
+		var bullets;
+		var laser;
+		var bulletTime;
+		var bullet;
+		var game;
+		var player;
 
-    it('#fire', function(){
-      anotherPlayer.fire(bullets, gameAudio.add, fireButton);
-      //expect(fireButton.isDown).toEqual(true);
-    });
+		beforeEach(function(){
+			fireButton = {
+				isDown: false
+			};
 
-    it('#firBullets',function(){
-      anotherPlayer.fireBullet(bullets, gameTime, player);
-    })
+			player = {
+				alive: true,
+				x: 5,
+				y: 5
+			};
 
-  });
+			bullets = {
+				getFirstExists: function(value){
+					return value;
+				}
+			};
 
+			laser = {
+				play: {
+					function() {}
+				}
+			};
+
+			game = {
+				time: {
+					now: 10
+				},
+				add: {
+					sprite: function(v1, v2, v3){}
+				},
+				physics: {
+					arcade: {
+						enable: function(value){}
+					}
+				},
+				world: {
+					centerX: 100,
+					centerY: 200
+				}
+			};
+
+			bullet = {
+				reset: function (value1, value2) {},
+				body: {
+					velocity: {
+						y: 1000
+					}
+				}
+			};
+
+			bulletTime = 0;
+			anotherPlayer = new Player(game);
+
+			spyOn(laser, 'play');
+			spyOn(bullets, 'getFirstExists').and.returnValue(bullet);
+			spyOn(bullet, 'reset' );
+		});
+
+		it('sets the correct properties in #fireBullet', function(){
+			fireButton.isDown = true;
+			anotherPlayer.fire(bullets, laser, fireButton, game, player);
+			expect(bullet.reset).toHaveBeenCalled();
+			expect(bullet.body.velocity.y).toEqual(-400);
+		});
+
+		it('plays the laser animation sound', function(){
+			fireButton.isDown = true;
+			anotherPlayer.fire(bullets, laser, fireButton, game, player);
+			expect(laser.play).toHaveBeenCalledTimes(1);
+		});
+	});
 
 });
