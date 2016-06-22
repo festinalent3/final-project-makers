f2ac95ec09f642a62d2302079be267e6e29233ef
Aday Mesa <adaymesagc@gmail.com>
2016-06-22 12:36:40 +0100

    First test pass

---
+++ test/backgroundSpec.js
@@ -0,0 +1,56 @@
+import * as background from '../src/modules/background';
+
+describe('background', function() {
+  var bckArray = ["bck_1", "bck_2", "bck_3", "bck_4", "bck_5", "bck_6", "bck_1"];
+  var level = 1;
+  var game;
+  var currentLVL;
+  var backgroundVelocity;
+  var currentBck;
+
+  beforeEach(function(){
+    currentLVL = 1;
+    backgroundVelocity = 5;
+    level = 1;
+
+    currentBck = {
+      loadTexture: function(x,y) {},
+      key:""
+      
+    };
+
+      game = {
+        add: {
+          tileSprite: function(x,y,z,w,key){
+            return key;
+        }
+      }
+    };
+
+    spyOn(currentBck, 'loadTexture').and.returnValue(currentBck.key=bckArray[level-1]);
+    spyOn(game.add, 'tileSprite').and.returnValue(currentBck);
+
+    background.reset();
+  });
+
+
+  it('resets the background', function(){
+    background.create(game)
+    background.update(level);
+    expect(background.get()).toEqual(currentBck);
+    background.reset();
+    expect(background.get().key).toEqual(bckArray[0]);
+  });
+
+  // it('kills bullet and enemy', function(){
+  //   score.update(bullet, enemy);
+  //   expect(bullet.kill).toHaveBeenCalled();
+  //   expect(enemy.kill).toHaveBeenCalled();
+  // });
+
+  // it('increases the score', function(){
+  //   score.update(bullet, enemy);
+  //   expect(score.get()).toEqual(scoreIncrease);
+  // });
+
+});
